79b54d9c52d7b807cf80d10cad32464590f52bf2
chayma.abdelkefi <chayma.abdelkefi@enis.tn>
2021-05-10 05:22:49 +0100

    second commit

---
+++ workout.c
@@ -0,0 +1,18 @@
+h1{
+    font-size:60px;
+    color:red;
+    text-decoration: underline;
+ }
+ p{
+     color:green;
+ }
+ .titre{
+     color:yellow;
+ }
+  .div1{
+     background-color: blue;
+     margin-top:200px;
+ }
+ .div2{
+     background-color: burlywood;
+ }
